96708de1114bfa1f43ebfb1e79eae1e6f71974eb
rongtong <jinrongtong5@163.com>
2022-12-21 10:36:57 +0800

    [ISSUE #5712] Fix the invalid of heartbeat detection after controller switch (#5711)
    * Fix the invalid of heartbeat detection after controller switch
    
    * Pass the checkstyle
    
    * Format ReplicasInfoManagerTest code style

--- broker/src/main/java/org/apache/rocketmq/broker/BrokerController.java
+++ broker/src/main/java/org/apache/rocketmq/broker/BrokerController.java
@@ -1736,7 +1736,9 @@ public class BrokerController {
                         this.brokerConfig.getSendHeartbeatTimeoutMillis(),
                         this.brokerConfig.isInBrokerContainer(), this.replicasManager.getLastEpoch(),
                         this.messageStore.getMaxPhyOffset(),
-                        this.replicasManager.getConfirmOffset()
+                        this.replicasManager.getConfirmOffset(),
+                        this.brokerConfig.getControllerHeartBeatTimeoutMills(),
+                        this.brokerConfig.getBrokerElectionPriority()
                     );
                 }
             }
