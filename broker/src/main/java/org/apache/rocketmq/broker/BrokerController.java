d4b7204b774ea831e922698cb3cd0e24ce9dac42
TheR1sing3un <ther1sing3un@163.com>
2023-02-07 00:57:31 +0800

    fix(controller): fix some bug to pass AutoSwitchRoleIntegrationTest
    1. fix some bug to pass AutoSwitchRoleIntegrationTest

--- broker/src/main/java/org/apache/rocketmq/broker/BrokerController.java
+++ broker/src/main/java/org/apache/rocketmq/broker/BrokerController.java
@@ -1746,7 +1746,7 @@ public class BrokerController {
                         this.brokerConfig.getBrokerClusterName(),
                         this.getBrokerAddr(),
                         this.brokerConfig.getBrokerName(),
-                        this.brokerConfig.getBrokerId(),
+                        this.replicasManager.getBrokerId(),
                         this.brokerConfig.getSendHeartbeatTimeoutMillis(),
                         this.brokerConfig.isInBrokerContainer(), this.replicasManager.getLastEpoch(),
                         this.messageStore.getMaxPhyOffset(),
