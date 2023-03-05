22e2e40a5421af2a8483cfd369b4184afa44d10f
TheR1sing3un <ther1sing3un@163.com>
2023-03-05 12:16:19 +0800

    fix(controller): remove unused import
    1. remove unused import

--- controller/src/main/java/org/apache/rocketmq/controller/impl/manager/SyncStateInfo.java
+++ controller/src/main/java/org/apache/rocketmq/controller/impl/manager/SyncStateInfo.java
@@ -19,7 +19,6 @@ package org.apache.rocketmq.controller.impl.manager;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
-import org.apache.commons.lang3.StringUtils;
 
 /**
  * Manages the syncStateSet of broker replicas.
